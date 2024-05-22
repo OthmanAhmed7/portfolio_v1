3607c525396ab1d274afc7554b80f74b1ed8ad40
Othman Ahmed <105116176+OthmanAhmed7@users.noreply.github.com>
2024-05-22 18:47:34 +0300

    Add files via upload

--- layout.tsx
+++ layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
   weight: ["300", "400", "600", "700", "800"],
@@ -21,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>{children}</body>
-      <Analytics />
     </html>
   );
 }
